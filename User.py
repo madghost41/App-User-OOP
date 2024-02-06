749b3c851d50a481ec52f5c9a04f1578bfc6e524
madghost41 <stephenmicheal97@gmail.com>
2024-02-06 11:21:07 -0600

    second commit

--- User.py
+++ User.py
@@ -1,1 +1,15 @@
-# your User class goes here
+# your User class goes hereclass User:
+class User():
+    def __init__(self, name, email, address, password):
+        self.name = name
+        self.email = email
+        self.address = address
+        self.password = password
+    
+    def __str__(self):
+        return f"I am {self.name} and my email is {self.email}"
+
+   
+
+kurt = User('k.angle', 'kangel@gmail.com', '123 Drury Lane', 'password123')
+print(kurt)
